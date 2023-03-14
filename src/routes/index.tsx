0769355d1f19767cc5ab67b597f4e68547dcebc7
Lucélia Batista <lucelia.ti@gmail.com>
2023-03-14 11:34:42 -0300

    Rota corrigida

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -10,23 +10,6 @@ export const AppRoutes = () => {
       <Route path="/login/admin" element={<LoginAdmin />} />
       <Route path="/login/customer" element={<LoginCustomer />} />
       <Route path="/cadastro" element={<Register />} />
-      <Route
-        path="/excluir"
-        element={
-          <ModalDelete
-            isOpen={false}
-            toggle={function (): void {
-              throw new Error("Function not implemented.");
-            }}
-            cancel={function (): void {
-              throw new Error("Function not implemented.");
-            }}
-            confirm={function (data: any): void {
-              throw new Error("Function not implemented.");
-            }}
-          />
-        }
-      />
     </Routes>
   );
 };
